7397f2845694a90ac846f7fba2ceb90bf4c4b3f6
weiitang <107684428+weiitang@users.noreply.github.com>
2024-04-11 09:11:53 +0000

    Deploying to gh-pages from @ weiitang/icon@9953efb3cf98f3bc163750e2f74c9ef4105e14d4 🚀

--- babel.cjs.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-transform-modules-commonjs',
-    '@babel/plugin-transform-arrow-functions',
-  ],
-};
